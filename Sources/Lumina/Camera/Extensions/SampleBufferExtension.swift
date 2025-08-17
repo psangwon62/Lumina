f0996fb04c8e05df40d29c795ec008036ffb39aa
psangwon62 <psangwon62@gmail.com>
2025-08-18 04:27:01 +0900

    fix: 모델 사용 하지 않고 stream만 하는 경우 발생하는 메모리 초과 문제 해결

--- Sources/Lumina/Camera/Extensions/SampleBufferExtension.swift
+++ Sources/Lumina/Camera/Extensions/SampleBufferExtension.swift
@@ -10,19 +10,30 @@ import AVFoundation
 import UIKit
 
 extension CMSampleBuffer {
-    func normalizedVideoFrame(forCamera: CameraPosition) -> UIImage? {
-        LuminaLogger.notice(message: "normalizing video frame from CMSampleBbuffer")
-        guard let imageBuffer = CMSampleBufferGetImageBuffer(self) else {
-            return nil
-        }
-        let coreImage = CIImage(cvPixelBuffer: imageBuffer)
-        let context = CIContext()
-        guard let sample: CGImage = context.createCGImage(coreImage, from: coreImage.extent) else {
-            return nil
-        }
-        return UIImage(cgImage: sample, scale: 1.0, orientation: getImageOrientation(forCamera: forCamera))
-    }
+    private static let ciContext = CIContext()
 
+    func normalizedVideoFrame(forCamera: CameraPosition) -> UIImage? {
+        return autoreleasepool {
+            LuminaLogger.notice(message: "normalizing video frame from CMSampleBuffer")
+
+            guard let imageBuffer = CMSampleBufferGetImageBuffer(self) else {
+                return nil
+            }
+
+            let coreImage = CIImage(cvPixelBuffer: imageBuffer)
+
+            guard let sample: CGImage = CMSampleBuffer.ciContext.createCGImage(coreImage, from: coreImage.extent) else {
+                return nil
+            }
+
+            let result = UIImage(
+                cgImage: sample,
+                scale: 1.0,
+                orientation: getImageOrientation(forCamera: forCamera)
+            )
+            return result
+        }
+    }
     private func getImageOrientation(forCamera: CameraPosition) -> UIImage.Orientation {
         switch LuminaViewController.orientation {
             case .landscapeLeft:
